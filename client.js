6f0d4b577293c8b86749ca2e0660e3d6032e92e2
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-10-23 14:14:07 -0700

    add file for constant variables

--- client.js
+++ client.js
@@ -1,14 +1,15 @@
 const net = require('net');
+const {IP, PORT} = require('./constants');
 
 
 const connect = () => {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   conn.setEncoding('utf8');
-  
+
   return conn;
 
 };
