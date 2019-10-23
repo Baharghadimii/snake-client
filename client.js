5916d6b596afdb70dc52a71e781763b5e02ed07d
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-10-23 12:59:17 -0700

    add stdin

--- client.js
+++ client.js
@@ -1,41 +1,61 @@
 const net = require('net');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput(stdin);
+  return stdin;
+};
+const handleUserInput = (key) => {
+  key.on('data', data => {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
 const connect = () => {
   const conn = net.createConnection({
-    host: 'localhost',
+    host: '192.168.88.149',
     port: 50541
   });
 
   conn.setEncoding('utf8');
 
+  setupInput();
+
+
   conn.on('data', (data) => {
     console.log(data);
   });
-  conn.on('connect' , () => {
-    console.log('Successfully connected to game server');
-    conn.write('Name: BGH');
-    setTimeout(() => {
-      conn.write('Move: up');
-    } , 500);
-    setTimeout(() => {
-      conn.write('Move: up');
-    } , 700);
-    setTimeout(() => {
-      conn.write('Move: up');
-    } , 900);
-    setTimeout(() => {
-      conn.write('Move: right');
-    } , 1100);
-    setTimeout(() => {
-      conn.write('Move: right');
-    } , 1300);
-    setTimeout(() => {
-      conn.write('Move: right');
-    } , 1500);
-    
-  });
+  // conn.on('connect', () => {
+  //   console.log('Successfully connected to game server');
+  //   conn.write('Name: BGH');
+  //   setTimeout(() => {
+  //     conn.write('Move: up');
+  //   }, 500);
+  //   setTimeout(() => {
+  //     conn.write('Move: up');
+  //   }, 700);
+  //   setTimeout(() => {
+  //     conn.write('Move: up');
+  //   }, 900);
+  //   setTimeout(() => {
+  //     conn.write('Move: right');
+  //   }, 1100);
+  //   setTimeout(() => {
+  //     conn.write('Move: right');
+  //   }, 1300);
+  //   setTimeout(() => {
+  //     conn.write('Move: right');
+  //   }, 1500);
+
+  // });
 
   return conn;
+
 
 
 };
